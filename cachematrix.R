fe71c714bb270ed955635e511966caa727c88c40
polanict <polanict@gmail.com>
2015-06-21 15:53:22 -0500

    First Attempt
    First attempt

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,38 @@
 ## functions do
 
 ## Write a short comment describing this function
+##
+## Function makeCacheMatrix will create an matrix that is invertable, calculate the inverse and store it to cache.
 
 makeCacheMatrix <- function(x = matrix()) {
+      m <- NULL
+      set <- function(y){
+            x <<- y
+            m <<- NULL
+      }
+      get <- function()x
+      setinv <- function(s) m<<-solve(s)
 
 }
 
 
 ## Write a short comment describing this function
+##
+## Function casheSolve will solve the inverse of the matrix if it is not already in cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        ##
+      cachemean <- function(x, ...) {
+            m <- x$setinv()
+            if(!is.null(m)) {
+                  message("getting cached data")
+                  return(m)
+            }
+            data <- x$get()
+            m <- solve(data, ...)
+            x$setinv(m)
+            m
+      }
+
 }
